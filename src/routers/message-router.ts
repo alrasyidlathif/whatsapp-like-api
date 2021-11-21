6115c2d6a6d831ba6b4229a1655bcccaf42fb626
alrasyidlathif <alrasyidlathif@gmail.com>
2021-11-21 23:01:24 +0700

    FIX: make route easier

--- src/routers/message-router.ts
+++ src/routers/message-router.ts
@@ -3,7 +3,7 @@ import messageController from '../controllers/message-controller'
 
 class UserRouter extends BaseRouter {
     public routes(): void {
-        this.router.post('/:sender_id/to/:receiver_id', messageController.sendMessage)
+        this.router.post('/:user_id1/to/:user_id2', messageController.sendMessage)
         this.router.get('/:user_id1/and/:user_id2', messageController.getMessages)
         this.router.get('/:user_id1', messageController.getAllMessages)
     }
